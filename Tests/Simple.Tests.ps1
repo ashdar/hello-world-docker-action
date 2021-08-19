b3edb5b35edbb0fbee0989ba3aaf8a83f9ad4cf1
Darin Strait <Darin.Strait@ashdar-partners.com>
2021-08-19 16:13:20 -0400

    Add a SQL test.

--- Tests/Simple.Tests.ps1
+++ Tests/Simple.Tests.ps1
@@ -9,3 +9,24 @@ Describe "Foo" {
         }
     }
 }
+
+
+Describe "SQL Checks" {
+
+    # Where are we going?
+    $ServerInstance = 'localhost'
+
+    # Need to build a $Credential that I cn log into the SQL Server with
+    $Username = 'sa'
+    $Password = $env:SA_PASSWORD
+    $SecureString = ConvertTo-SecureString -AsPlainText $Password -Force
+    $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username,$SecureString
+
+    context "Is SQL alive" {
+        It "is connectible" {
+            $Query = "select getdate() RightNow"
+            $Result = Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Credential $Cred -Query $Query
+            ($Result).Count | Should -BeGreaterThan 0
+        }
+    }
+}
