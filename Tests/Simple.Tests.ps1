5f91d9a9ef06e5d3b82d1bbe2abafaf61563c12b
Darin Strait <Darin.Strait@ashdar-partners.com>
2021-08-19 16:49:42 -0400

    Debuging, again.

--- Tests/Simple.Tests.ps1
+++ Tests/Simple.Tests.ps1
@@ -1,7 +1,7 @@
 
-Describe "Foo" {
-    context "Bar" {
-        It "is true" {
+Describe 'Foo' {
+    Context 'Bar' {
+        It 'is true' {
             $true | Should -BeTrue
         }
         It "ain't true should force a failure" {
@@ -11,29 +11,29 @@ Describe "Foo" {
 }
 
 
-Describe "SQL Checks" {
-
-    # FIXME: This set up code would be better using BeforeAll/BeforeEach. This is just a demo.
-
-    # Where are we going?
-    if ($ENV:TEST_SQLINSTANCE) {
-        $ServerInstance = $ENV:TEST_SQLINSTANCE
-    }
-    else {
-        $ServerInstance = 'localHost'
-    }
-
-    # Need to build a $Credential that I cn log into the SQL Server with
-    $Username = 'sa'
-    $Password = $env:SA_PASSWORD
-    $SecureString = ConvertTo-SecureString -AsPlainText $Password -Force
-    $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username,$SecureString
+Describe 'SQL Checks' {
 
     Context "Is SQL alive: $ServerInstance" {
-        It "is connectible" {
-            $Query = "select getdate() RightNow"
-            $Result = Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Credential $Cred -Query $Query
-            ($Result).Count | Should -BeGreaterThan 0
+        # FIXME: This set up code would be better using BeforeAll/BeforeEach. This is just a demo.
+
+        # Where are we going?
+        if ($ENV:TEST_SQLINSTANCE) {
+            $ServerInstance = $ENV:TEST_SQLINSTANCE
+        }
+        else {
+            $ServerInstance = 'localHost'
+        }
+
+        # Need to build a $Credential that I cn log into the SQL Server with
+        $Username = 'sa'
+        $Password = $env:SA_PASSWORD
+        $SecureString = ConvertTo-SecureString -AsPlainText $Password -Force
+        $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username, $SecureString
+
+        It 'is connectible' {
+            $Query = 'select getdate() RightNow'
+            $Result = @(Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Credential $Cred -Query $Query -AppendServerInstance)
+            $Result.Count | Should -BeGreaterThan 0
         }
     }
 }
